f8d3be39b71faa44871c18e8b2b089253770c37e
Sachin Gupta <sachin_gupta@rsystems.com>
2018-07-05 19:50:27 +0530

    DIS_Integration_Project

---
+++ DIS_Integration/src/main/java/com/carrieriq/DIS_Integration/Constants.java
@@ -0,0 +1,26 @@
+package com.carrieriq.DIS_Integration;
+
+public class Constants {
+
+
+    public static final String URL = "URL";
+    public static final String USER = "USER";
+    public static final String PASS = "PASS";
+
+    public static final String SEDIS_CONFIG_TESTS = "options.properties";
+
+    public static final String HUB_HOST = "hub_host";
+
+//    public static final String BROWSER = "browser";
+//    public static final String LOCAL_RUN = "local_run";
+//    public static final String TRUE = "true";
+    public static final String FIREFOX = "firefox";
+    public static final String INTERNET_EXPLORER = "internetExplorer";
+    public static final String IE_DRIVER_PATH = "ie_driver_path";
+    public static final String WEBDRIVER_IE_DRIVER = "webdriver.ie.driver";
+    public static final String GECKO_DRIVER_PATH= "gecko_driver_path";
+    public static final String WEBDRIVER_GECKO_DRIVER="webdriver.gecko.driver";
+
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+
+}
